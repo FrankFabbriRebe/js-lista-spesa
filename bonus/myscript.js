8854a9ecf7393a9cab47db53a3da1cb1550743c7
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2023-11-24 16:40:06 +0100

    add: inner container

--- bonus/myscript.js
+++ bonus/myscript.js
@@ -7,6 +7,16 @@ const startButton = document.querySelector(".start");
 // when click start button
 startButton.addEventListener ("click", function() {
 
+    // add hidden class to start button
     startButton.classList.add("hidden");
+
+    // create inner container
+    const innerContainer = document.createElement("div");
+
+    // add class to inner container
+    innerContainer.classList.add("inner_container");
+
+    // append inner containewr to container
+    container.append(innerContainer);
 
 })
