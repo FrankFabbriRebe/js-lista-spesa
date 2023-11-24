0cb3ada3cb4e6c770108fe7e0232cd45bee5cca4
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2023-11-24 14:35:03 +0100

    feat: while cicle

--- myscript.js
+++ myscript.js
@@ -1,3 +1,14 @@
 // create array lista spesa
 const listaSpesa = ["Pomodori", "Mele", "Insalata", "Carote"];
-console.log(listaSpesa);
+//console.log(listaSpesa);
+
+let i = 0;
+
+// cicle while
+while (i < listaSpesa.length) {
+
+    console.log(listaSpesa[i]);
+
+    i++
+
+}
