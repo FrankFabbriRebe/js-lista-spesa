511a41302c441da3a4ac4e1af759c2b43c131a82
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2023-11-24 14:55:20 +0100

    feat: when click start button add class

--- bonus/myscript.js
+++ bonus/myscript.js
@@ -3,3 +3,10 @@ const container = document.querySelector(".container");
 
 // add start button
 const startButton = document.querySelector(".start");
+
+// when click start button
+startButton.addEventListener ("click", function() {
+
+    startButton.classList.add("hidden");
+
+})
